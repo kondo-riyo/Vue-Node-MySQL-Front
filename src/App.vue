e8c578775b657c4ced5bf3b2a1641a1ff7a10274
kondo-riyo <r1y0.tenten.1126@gmail.com>
2021-11-24 09:25:34 +0900

    vuetifyの導入

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <!-- <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/> -->
-  <!-- </div> -->
-  <div>
+  <v-app>
     <login v-if="!isLogin"></login>
     <home v-else />
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -28,25 +21,4 @@ export default {
   },
 };
 </script>
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
-</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,33 @@
 <template>
   <div class="login">
-    <div class="fill-height" fluid>
-      <div
+    <v-container class="fill-height" fluid>
+      <v-row
         align="center"
         justify="center"
+        class="headline font-italic font-weight-medium text--secondary"
       >
-        <div cols="12" sm="8" md="6" lg="4">
-          ChatApp
-        </div>
-      </div>
-      <div align="center" justify="center">
-        <div cols="12" sm="8" md="6" lg="4">
-          <div class="elevation-12">
-            <div color="primary" dark flat>
-              <div>Login form</div>
-              <div></div>
-            </div>
-            <div>
-              <div>
-                <!-- <input
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-icon large>mdi-chat-processing-outline</v-icon>ChatApp
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Login form</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
                   label="UserId"
                   name="login"
                   prepend-icon="mdi-account"
                   type="text"
                   v-model="userId"
-                /> -->
-                <input
-                  name="login"
-                  type="text"
-                  v-model="userId"
-                />
+                ></v-text-field>
 
-                <!-- <input
+                <v-text-field
                   id="password"
                   label="Password"
                   name="password"
@@ -41,23 +37,18 @@
                   @click:append="showPassword = !showPassword"
                   @keyup.enter="login"
                   v-model="password"
-                /> -->
-                <input
-                  id="password"
-                  name="password"
-                  v-model="password"
-                />
-              </div>
-            </div>
-            <div>
-              <div align="center" justify="space-around">
-                <button color="#B2EBF2" @click="login">Login</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-row align="center" justify="space-around">
+                <v-btn color="#B2EBF2" @click="login">Login</v-btn>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
